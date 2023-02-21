df4fa40df4174ea7f216bdece41d81ddf3695cc4
arsalan <arsalan072977@gmail.com>
2023-02-21 12:19:46 -0800

    add

---
+++ script.js
@@ -0,0 +1,10 @@
+let audio = document.createElement("audio");
+audio.setAttribute("src", "sound2.mp3");
+audio.loop = true;
+// audio.play();
+let botton = getElementById("btm-1")
+let x;
+function forward(){
+    
+
+}
